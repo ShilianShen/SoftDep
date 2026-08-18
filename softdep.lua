a912af5e93c0a910708c7232db5aa5a50251dd39
阿沈 <149996372+ShilianShen@users.noreply.github.com>
2026-08-18 07:14:36 +0200

    Refactor: enhance kahn function with improved validation and restructure parent-child relationships in adopt function

--- softdep.lua
+++ softdep.lua
@@ -1,3 +1,56 @@
+package.preload["kahn"] = function(...)
+	local function kahn(vertices, pkey)
+		assert(type(vertices) == "table")
+		assert(type(pkey) == "string")
+		for tag, vertex in pairs(vertices) do
+			assert(type(tag) == "string")
+			assert(type(vertex) == "table")
+			assert(type(vertex[pkey]) == "table")
+			for ptag, _ in pairs(vertex[pkey]) do
+				assert(type(ptag) == "string")
+			end
+		end
+
+		local infos = {}
+		local stack = {}
+		local order = {}
+		local count = 0
+
+		for tag, _ in pairs(vertices) do
+			infos[tag] = { indegree = 0, children = {} }
+			count = count + 1
+		end
+
+		for tag, vertex in pairs(vertices) do
+			for ptag, _ in pairs(vertex[pkey]) do
+				assert(infos[ptag] ~= nil)
+				assert(infos[ptag].children[tag] ~= true)
+				infos[tag].indegree = infos[tag].indegree + 1
+				infos[ptag].children[tag] = true
+			end
+			if infos[tag].indegree == 0 then
+				table.insert(stack, tag)
+			end
+		end
+
+		for _ = 1, count do
+			assert(#stack ~= 0)
+			local tag = table.remove(stack)
+			for ctag, _ in pairs(infos[tag].children) do
+				infos[ctag].indegree = infos[ctag].indegree - 1
+				if infos[ctag].indegree == 0 then
+					table.insert(stack, ctag)
+				end
+			end
+			table.insert(order, tag)
+		end
+
+		return order
+	end
+
+	return kahn
+end
+
 local softdep = {}
 
 local ACCESS = {
@@ -12,53 +65,33 @@ local MT = {
 	end,
 }
 
-local function kahn(vertices, pkey)
+local kahn = require("kahn.lua")
+
+local function adopt(vertices, pkey, ckey)
 	assert(type(vertices) == "table")
 	assert(type(pkey) == "string")
+	assert(type(ckey) == "string")
 	for tag, vertex in pairs(vertices) do
 		assert(type(tag) == "string")
 		assert(type(vertex) == "table")
-		assert(vertex[pkey] == nil or type(vertex[pkey]) == "table")
-		for ptag, _ in pairs(vertex[pkey] or {}) do
+		assert(type(vertex[pkey] == "table"))
+		for ptag, _ in pairs(vertex[pkey]) do
 			assert(type(ptag) == "string")
+			assert(vertices[ptag] ~= nil)
 		end
-	end
-
-	local infos = {}
-	local stack = {}
-	local order = {}
-	local count = 0
-
-	for tag, _ in pairs(vertices) do
-		infos[tag] = { indegree = 0, children = {} }
-		count = count + 1
 	end
 
 	for tag, vertex in pairs(vertices) do
-		for ptag, _ in pairs(vertex[pkey] or {}) do
-			assert(infos[ptag] ~= nil)
-			assert(infos[ptag].children[tag] ~= true)
-			infos[tag].indegree = infos[tag].indegree + 1
-			infos[ptag].children[tag] = true
-		end
-		if infos[tag].indegree == 0 then
-			table.insert(stack, tag)
-		end
+		vertex[ckey] = {}
 	end
 
-	for _ = 1, count do
-		assert(#stack ~= 0)
-		local tag = table.remove(stack)
-		for ctag, _ in pairs(infos[tag].children) do
-			infos[ctag].indegree = infos[ctag].indegree - 1
-			if infos[ctag].indegree == 0 then
-				table.insert(stack, ctag)
-			end
+	for tag, vertex in pairs(vertices) do
+		for ptag, _ in pairs(vertex[pkey]) do
+			local parent = vertices[ptag]
+			vertex[pkey][ptag] = parent
+			parent[ckey][tag] = vertex
 		end
-		table.insert(order, tag)
 	end
-
-	return order
 end
 
 local function array2set(array)
@@ -142,6 +175,7 @@ local function newNode(data, tasks, access)
 	end
 
 	node.taskOrder = kahn(node.tasks, "parents")
+	adopt(node.tasks, "parents", "children")
 
 	return node
 end
@@ -157,6 +191,7 @@ local function loadGraph(graph)
 	assert(graph ~= nil)
 	assert(not graph.ready)
 	graph.nodeOrder = kahn(graph.nodes, "parents")
+	adopt(graph.nodes, "parents", "children")
 	graph.ready = true
 end
 
