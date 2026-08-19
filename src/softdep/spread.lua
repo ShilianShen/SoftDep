cdee0b0424966147211d55b011e5f07f73609d59
阿沈 <149996372+ShilianShen@users.noreply.github.com>
2026-08-19 02:12:39 +0200

    Refactor: reorganize spread function for improved clarity and maintainability

--- src/softdep/spread.lua
+++ src/softdep/spread.lua
@@ -1,29 +1,38 @@
-local function spread(graph)
-	for _, ntag in ipairs(graph.ntagOrder) do
-		local node = graph.nodes[ntag]
-		local dirty = false
-		for _, ttag in ipairs(node.ttagOrder) do
-			local task = node.tasks[ttag]
-			if task.dirty then
-				for _, ctask in pairs(task.children) do
-					ctask.dirty = true
-				end
-				if task.access == "writable" then
-					dirty = true
+local function spreadInNode(node)
+	local dirty = false
+	for _, ttag in ipairs(node.ttagOrder) do
+		local task = node.tasks[ttag]
+		if task.dirty then
+			for _, ctask in pairs(task.children) do
+				ctask.dirty = true
+			end
+			if task.access == "writable" then
+				dirty = true
+			end
+		end
+	end
+	return dirty
+end
+
+local function spreadOutNode(ntag, node)
+	for _, cnode in pairs(node.children) do
+		for _, task in pairs(cnode.tasks) do
+			for _, cntag in pairs(task.args) do
+				if cntag == ntag then
+					task.dirty = true
+					break
 				end
 			end
 		end
+	end
+end
+
+local function spread(graph)
+	for _, ntag in ipairs(graph.ntagOrder) do
+		local node = graph.nodes[ntag]
+		local dirty = spreadInNode(node)
 		if dirty then
-			for _, cnode in pairs(node.children) do
-				for _, ctask in pairs(cnode.tasks) do
-					for catag, cntag in pairs(ctask.args) do
-						if cntag == ntag then
-							ctask.dirty = true
-							break
-						end
-					end
-				end
-			end
+			spreadOutNode(ntag, node)
 		end
 	end
 end
